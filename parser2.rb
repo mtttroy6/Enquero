12b647e269db1c9831f450a9eef00c762011bc65
kelrezo <travis_layne@yahoo.com>
2015-11-09 23:18:40 -0600

    parser update
    made some minor changes still need to find out how to distinguish
    repeating tags

--- parser2.rb
+++ parser2.rb
@@ -7,32 +7,20 @@ class MyListener
   @@xmlHash = Hash.new
   @@data = ""
   @@path = Array.new
-  #@@previous path=[]
-  #@@currentpath =[]
-  #@@nextpath = []
   include REXML::StreamListener
   def tag_start(*args)
-  	#puts "tag_start: #{args[0]}"	
-    #$x = args[0]   
+    puts "#{@@path}"
    @@xmlHash[args[0]]= args[1..5]
-
    @@path.push(args[0])
-   
   end
   def tag_end(*args)
-
-    puts "#{@@path}"
-
-   # puts "#{args[0]} #{@@xmlHash[args[0]]}"
+  	puts "#{@@path}"
    @@path.pop
-  
-
   end
   def text(data)
     return if data =~ /^\w*$/     # whitespace only
     abbrev = data[0..200]
     @@data =abbrev
-  # puts data 
  end
   def output
   end
@@ -42,4 +30,3 @@ list = MyListener.new
 x = "testfile.xml"
 xmlfile = File.new(x)
 Document.parse_stream(xmlfile, list)
-#list.output
