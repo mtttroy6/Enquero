3d9f5651bd0f5e412c12395443174f0291bb1efb
kelrezo <travis_layne@yahoo.com>
2015-11-11 00:12:35 -0600

    Clean up
    Cleaned up the file a little bit, but still working on uniquely
    identifying the tags..

--- Enquero/parser2.rb
+++ Enquero/parser2.rb
@@ -6,71 +6,55 @@ info = Hash.new
 class Map
   @@path = Array.new
   @@id =""
+  @@data=""
 def initialize(path,id)
     @@path= path
     @@id=id
 end
 end
 class MyListener
-  @@xmlHash = Hash.new
-  @@data = ""
-  @@path = Array.new
-  @@id=1
-  @@startCount=0
-  @@endCount=0
-  @signal = false
+  @@xmlHash = Hash.new #dictionay for data
+  @@data = "" # grabs data from tags
+  @@path = Array.new #keep track of path
+  @@id=1 #id used for mapping
+  @@startCount=0 #count for start tags
+  @@endCount=0 # count for endtags
   @@tag =[]
+  @@idpath = Array.new
+  @@currentid=1
+  @@currenttag=""
   include REXML::StreamListener
+  #this methdos is calls for starting tags <tag>
   def tag_start(*args)
-    
-    #puts "#{@@xmlHash.keys}"
-    #puts "-----------------------"
-   #check for current tag if its already in the path update id
-   #
-   #
-   #
     @@startCount +=1
     if @@endCount > 1
       if @@xmlHash.has_key?(args[0])
-         #puts "#{args[0]}"
-         @@id+=1
-      else
-         #puts"#{args[0]}"  
+         @@currentid+=1
       end
     end
-    #puts "#{@@id}"
+    @@idpath.push(@@currentid)
     @@endCount=0 
     @@xmlHash[args[0]]=args[1..args.size]
     @@path.push(args[0])
-    @@signal = false
     @@tag.push(Map.new(@@path,@@id))
-    #puts "#{@@id}"
+    @@currenttag = args[0]
+    #puts "#{@@path} #{args[0]}"
   end
+  #this method is called on closing tags </tag>
   def tag_end(*args)
-    puts " path #{@@path}"
-    #puts "#{args[0]}"
-  #puts "#{@@path}"
- # puts "#{args[0]}"
-    #tag = Tag.new(@@path)
+    #puts "#{@@idpath} #{args[0]}"
     @@endCount +=1
-    if @@endCount > 1
-        #@@id +=1
-        @@signal = true
-    else
-    end
-    #puts "#{args}"
     @@startCount=0
-    @@path.pop
+    @@idpath.pop
+    @@curentid = @@path.pop
   end
+  #called when ecourtering dta betwwen tags <tag>data</tag>
+  #the if statment is to ignor the tags with no data <tag></tag>
   def text(data)
-    return if data =~ /^\w*$/     # whitespace only
-    abbrev = data[0..data.size]
-    @@data =abbrev
- end
-  def output
-    @@tag.each do |x|
-      puts "#{x.path} #{x.id}"
-    end 
+  	if data =~ /^\n*$/ 
+  	else    
+    puts "  text   :   #{data.inspect}"
+  end
   end
 end
 
@@ -78,4 +62,3 @@ list = MyListener.new
 x = "testfile.xml"
 xmlfile = File.new(x)
 Document.parse_stream(xmlfile, list)
-
